1d87525e3ab3c16bba4366af60f9a40de5ec84dc
Blend Statovci <blend@clleancode.com>
2026-07-29 15:26:39 +0200

    #20537-Add theme support for automatic feed links, responsive embeds, block styles, wide alignment, HTML5 elements, and custom logo in functions.php

--- functions.php
+++ functions.php
@@ -15,6 +15,31 @@
 		function balkan_nature_adventure_setup() {
 			add_theme_support( 'title-tag' );
 			add_theme_support( 'post-thumbnails' );
+			add_theme_support( 'automatic-feed-links' );
+			add_theme_support( 'responsive-embeds' );
+			add_theme_support( 'wp-block-styles' );
+			add_theme_support( 'align-wide' );
+			add_theme_support(
+				'html5',
+				array(
+					'search-form',
+					'comment-form',
+					'comment-list',
+					'gallery',
+					'caption',
+					'style',
+					'script',
+				)
+			);
+			add_theme_support(
+				'custom-logo',
+				array(
+					'height'      => 100,
+					'width'       => 400,
+					'flex-height' => true,
+					'flex-width'  => true,
+				)
+			);
 
 			register_nav_menus(
 				array(
