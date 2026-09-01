fee1ca98bbdca1f02a26613eb19fea2d81eef955
blendstatovci <129066633+blendstatovci@users.noreply.github.com>
2026-09-01 14:26:28 +0200

    Updated the block custom code

--- includes/blocks/block-custom-code.php
+++ includes/blocks/block-custom-code.php
@@ -1,5 +1,5 @@
 <div class="container">
     <div style="width: 100%;">
-        <?php echo wp_kses_post( get_field( 'custom_code' ) ); ?>
+        <?php echo get_field( 'custom_code' ) ; ?>
     </div>
 </div>
